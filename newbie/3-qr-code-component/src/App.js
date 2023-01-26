e3ca44d8940740717465a40a588586b7363a7c82
Furkan <furi544@gmail.com>
2023-01-26 15:38:28 +0300

    initial

---
+++ newbie/3-qr-code-component/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <img src="/image-qr-code.png" className="img" />
+        <div className="text-container">
+          <h3>Improve your front-end skills by building projects</h3>
+          <div>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
