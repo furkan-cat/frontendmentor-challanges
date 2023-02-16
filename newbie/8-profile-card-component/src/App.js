7bbe6a851a27d40d57d72ca268da30ff14d56654
Furkan <furi544@gmail.com>
2023-02-16 13:11:37 +0300

    initial

---
+++ newbie/8-profile-card-component/src/App.js
@@ -0,0 +1,45 @@
+import "./App.css";
+import BgPatternCard from "./bg-pattern-card";
+import BgPatternTop from "./bg-pattern-top";
+import BgPatternBottom from "./bg-pattern-bottom";
+function App() {
+  return (
+    <div className="wrapper">
+      <BgPatternTop className="bg-top-svg" />
+      <BgPatternBottom className="bg-bottom-svg" />
+      <div className="card-container">
+        <div className="image-container">
+          <img
+            src="/image-victor.jpg"
+            alt="victor"
+            className="image-container__item"
+          />
+        </div>
+        <div className="content-container">
+          <div>
+            <span className="content-container__title"> Victor Crest </span>
+            <span className="content-container__age">26</span>
+          </div>
+          <div className="content-container-location">London</div>
+          <hr />
+          <div className="footer">
+            <div>
+              <div className="bold">80K</div>
+              <div className="light">Followers</div>
+            </div>
+            <div>
+              <div className="bold">803K</div>
+              <div className="light">Likes</div>
+            </div>
+            <div>
+              <div className="bold">1.4K</div>
+              <div className="light">Photos</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
